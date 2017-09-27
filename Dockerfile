16bc658da7a3ba9e970d0f8bad530ee46053ea06
Tim Channell <tim.channell@gmail.com>
2017-09-26 22:11:16 -0400

    quick n' dirty no-hitler mode

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ FROM ubuntu:16.04
 WORKDIR /usr/local/src/bots
 
 RUN apt-get update
-RUN apt-get install -y python3 python3-pip
-RUN pip3 install ananas
+RUN apt-get install -y python3 python3-pip libjpeg-dev
+RUN pip3 install ananas Pillow numpy
 
 EXPOSE 443
 ADD . .
--- inspirobot/InspiroBot.py
+++ inspirobot/InspiroBot.py
@@ -1,24 +1,45 @@
+import io
 import requests
+import numpy as np
+from PIL import Image
 from inspirobot.quotes import quotes
 from ananas import PineappleBot, interval
 
 counter = 0
+reference = np.asarray(Image.open(open('./inspirobot/bad.jpg', 'rb'))).astype(np.float32)
+
+def compareImages(A, B):
+  error = np.absolute(A - B)
+  rmse = np.mean((error**2).flatten())**0.5
+  return (rmse <= 60) # are these images ~ similar?
 
 class InspiroBot(PineappleBot):
   @interval(1200)
   def post(self):
     global counter
+    global reference
+
     if (counter > len(quotes)):
       counter = 0
 
     generated = requests.get('http://inspirobot.me/api', {'generate': 'true'})
     if (generated.status_code == 200):
       imageData = requests.get(generated.text)
-      media = self.mastodon.media_post(imageData.content, "image/jpeg")
+      img = np.asarray(Image.open(io.BytesIO(imageData.content))).astype(np.float32)
 
+      if (compareImages(reference, img)):
+        print('image is evil, skipping')
+        return self.post()
+
+      print('posting')
+      media = self.mastodon.media_post(imageData.content, "image/jpeg")
       if (media['id'] != None):
         quote = quotes[counter]
         counter += 1
         self.mastodon.status_post(quote + " \n" + media['url'], media_ids=[media['id']])
     else:
       print('error')
+
+
+
+
